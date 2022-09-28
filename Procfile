555abbdc77b34406f8d4207bf39e332d6edb34da
ahmad940 <ahmadmuhammadmak5@gmail.com>
2022-09-28 21:23:40 +0100

    updated tables

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: npm run start:prod
-release: npx prisma db push
+# release: npx prisma db push
+release: npx prisma db push --accept-data-loss
--- prisma/migrations/20220928202245_init/migration.sql
+++ prisma/migrations/20220928202245_init/migration.sql
@@ -72,14 +72,17 @@ CREATE TABLE "adminProfiles" (
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
-    "uid" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
+    "userID" TEXT NOT NULL,
     "firstName" TEXT,
     "lastName" TEXT,
+    "email" TEXT NOT NULL,
+    "txPin" INTEGER,
+    "spID" TEXT,
+    "acct_balance" DOUBLE PRECISION DEFAULT 0.00,
     "password" TEXT NOT NULL,
     "isVerified" INTEGER,
     "isActive" INTEGER,
-    "verificationCode" TEXT,
+    "verificationCode" INTEGER,
     "role" "USER_ROLES" NOT NULL DEFAULT E'MEMBER',
     "onlineStatus" INTEGER NOT NULL DEFAULT 0,
     "registeredIp" TEXT,
@@ -90,6 +93,7 @@ CREATE TABLE "users" (
     "registeredBrowser" TEXT,
     "registeredOperatingSytsem" TEXT,
     "registeredDeviceType" TEXT,
+    "passwordChangedAt" TIMESTAMP(3),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -110,7 +114,7 @@ CREATE TABLE "userPasswordResets" (
 -- CreateTable
 CREATE TABLE "userProfiles" (
     "id" SERIAL NOT NULL,
-    "userId" TEXT NOT NULL,
+    "userID" TEXT,
     "phoneNumber" TEXT,
     "biography" TEXT,
     "address" TEXT,
@@ -124,20 +128,12 @@ CREATE TABLE "userProfiles" (
 );
 
 -- CreateTable
-CREATE TABLE "user_wallet" (
-    "id" SERIAL NOT NULL,
-    "uid" TEXT NOT NULL,
-    "balance" DOUBLE PRECISION DEFAULT 0.00,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "user_wallet_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "transaction_history" (
     "id" SERIAL NOT NULL,
-    "uid" TEXT NOT NULL,
+    "t_ref" TEXT NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "transaction_amount" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -161,19 +157,19 @@ CREATE UNIQUE INDEX "admins_email_key" ON "admins"("email");
 CREATE UNIQUE INDEX "adminProfiles_adminId_key" ON "adminProfiles"("adminId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "users_uid_key" ON "users"("uid");
+CREATE UNIQUE INDEX "users_userID_key" ON "users"("userID");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "userProfiles_userId_key" ON "userProfiles"("userId");
+CREATE UNIQUE INDEX "users_spID_key" ON "users"("spID");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_wallet_uid_key" ON "user_wallet"("uid");
+CREATE UNIQUE INDEX "userProfiles_userID_key" ON "userProfiles"("userID");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "transaction_history_uid_key" ON "transaction_history"("uid");
+CREATE UNIQUE INDEX "transaction_history_t_ref_key" ON "transaction_history"("t_ref");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Gallery_imageName_key" ON "Gallery"("imageName");
@@ -191,4 +187,10 @@ ALTER TABLE "admins" ADD CONSTRAINT "admins_roleId_fkey" FOREIGN KEY ("roleId") 
 ALTER TABLE "adminProfiles" ADD CONSTRAINT "adminProfiles_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "admins"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "userProfiles" ADD CONSTRAINT "userProfiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("uid") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "userProfiles" ADD CONSTRAINT "userProfiles_userID_fkey" FOREIGN KEY ("userID") REFERENCES "users"("userID") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transaction_history" ADD CONSTRAINT "transaction_history_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "users"("spID") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transaction_history" ADD CONSTRAINT "transaction_history_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users"("spID") ON DELETE RESTRICT ON UPDATE CASCADE;
